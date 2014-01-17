5011e13fb1d7e4ff52377595ec14cd00e55b8227
Antony MECHIN <antony.mechin@infinit.io>
2014-01-17 19:48:45 +0100

    fist.globals: Fix warning.

--- globals.hh
+++ globals.hh
@@ -57,7 +57,7 @@ namespace
 
 namespace regexp
 {
-  static QString email = "[a-z0-9\._%+-]+@[a-z0-9\.-]+\.[a-z]{2,4}";
+  static QString email = "[a-z0-9\\._%+-]+@[a-z0-9\\.-]+\\.[a-z]{2,4}";
 }
 
 namespace view
