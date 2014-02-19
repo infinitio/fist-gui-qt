76f516facc8e1dd1803bfe77cc6df57bf9b0b169
Antony MECHIN <antony.mechin@infinit.io>
2014-02-19 16:12:28 +0100

    main: Remove log.

--- src/fist-gui-qt/main.cc
+++ src/fist-gui-qt/main.cc
@@ -7,13 +7,9 @@
 #include <fist-gui-qt/Fist.hh>
 #include <fist-gui-qt/LoginWindow.hh>
 
-ELLE_LOG_COMPONENT("infinit.FIST");
-
 int
 main(int argc, char** argv)
 {
-  ELLE_LOG("start FIST gui");
-
   Fist application(argc, argv);
 
   return application();
