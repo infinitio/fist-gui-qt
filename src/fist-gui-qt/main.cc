972e3aa774b383d645e5cd3617711f3d1e4e6b11
Antony MECHIN <antony.mechin@infinit.io>
2014-03-28 23:35:34 +0100

    Revert "Force loading of reactor logger plugins."
    This reverts commit 559250bbc0917d29e2319513130cee0843584fff.

--- src/fist-gui-qt/main.cc
+++ src/fist-gui-qt/main.cc
@@ -2,20 +2,12 @@
 
 #include <QApplication>
 
-#include <elle/Plugin.hh>
 #include <elle/log.hh>
-
-#include <reactor/logger.hh>
 
 #include <surface/gap/gap.h>
 
 #include <fist-gui-qt/Fist.hh>
 #include <fist-gui-qt/LoginWindow.hh>
-
-elle::PluginLoad load_reactor_logger_plugins(
-  reactor::plugins::logger_indentation,
-  reactor::plugins::logger_tags
-  );
 
 int
 main(int argc, char** argv)
