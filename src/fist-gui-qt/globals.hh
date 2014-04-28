74f815f24a772a7f01cbcfba97c1602969ddeb0c
Antony MECHIN <antony.mechin@infinit.io>
2014-04-28 05:41:21 +0200

    utils: Handle email with up to 12 characters in the tld.

--- src/fist-gui-qt/globals.hh
+++ src/fist-gui-qt/globals.hh
@@ -59,7 +59,7 @@ namespace
 
 namespace regexp
 {
-  static QString email = "[a-z0-9\\._%+-]+@[a-z0-9\\.-]+\\.[a-z]{2,4}";
+  static QString email = "[a-z0-9\\._%+-]+@[a-z0-9\\.-]+\\.[a-z]{2,12}";
 }
 
 namespace view
