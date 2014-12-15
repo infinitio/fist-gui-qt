7ab00c0eb7b80681de891ec48f002000f3f356d5
Antony MECHIN <antony.mechin@infinit.io>
2014-12-15 10:01:28 +0100

    contextual: Fix some warnings.

--- src/contextual/stdafx.h
+++ src/contextual/stdafx.h
@@ -13,6 +13,7 @@
 // Insert your headers here
 # define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 
+# include <winsock2.h>
 # include <windows.h>
 # include <tchar.h>
 
