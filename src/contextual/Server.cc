8cc4e4207208528498153d0183bc9427093069ad
Antony MECHIN <antony.mechin@infinit.io>
2014-12-15 10:52:52 +0100

    contextual: Use the same log file than the app.

--- src/contextual/Server.cc
+++ src/contextual/Server.cc
@@ -5,7 +5,10 @@
 
 #include <elle/finally.hh>
 #include <elle/log.hh>
+#include <elle/os/environ.hh>
+#include <elle/log/TextLogger.hh>
 
+#include <common/common.hh>
 #include <contextual/Handler.hh>
 #include <contextual/helpers.hh>
 #include <contextual/HandlerFactory.hh>
@@ -22,11 +25,47 @@ DEFINE_GUID(
   CLSID_ContextMenu,
   0x256EF94C, 0x697D, 0x4986, 0xB9, 0x9B, 0x9D, 0x3B, 0x15, 0xD7, 0x9B, 0x49);
 
+static
+void
+initialize_logger()
+{
+  if (elle::os::inenv("INFINIT_LOG_FILE"))
+  {
+    auto* stream = new std::ofstream{elle::os::getenv("INFINIT_LOG_FILE"), std::fstream::app | std::fstream::out};
+    std::unique_ptr<elle::log::Logger> f(new elle::log::TextLogger(*stream, "TRACE", true, true, true, true, false));
+    elle::log::logger(std::move(f));
+  }
+}
+
+extern "C"
+__declspec(dllexport)
+HRESULT __stdcall
+DllMain(HMODULE hModule,
+          DWORD dwReason,
+          LPVOID lpReserved)
+{
+  switch (dwReason)
+  {
+    case DLL_PROCESS_ATTACH:
+      // Hold the instance of this DLL module, we will use it to get the
+      // path of the DLL to register the component.
+      g_DllInstance = hModule;
+      DisableThreadLibraryCalls(hModule);
+      break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+     break;
+  }
+  return TRUE;
+}
+
 extern "C"
 __declspec(dllexport)
 HRESULT __stdcall
 DllRegisterServer(void)
 {
+  initialize_logger();
   ELLE_TRACE_SCOPE("register server (lock: %s)", g_DllLockCounter);
   return S_OK;
 }
@@ -38,6 +77,7 @@ DllGetClassObject(REFCLSID rclsid,
                   REFIID riid,
                   void** ppv)
 {
+  initialize_logger();
   ELLE_TRACE_SCOPE("get class object for id %s", rclsid);
 
   if (!IsEqualIID(rclsid, CLSID_ContextMenu))
@@ -64,6 +104,7 @@ __declspec(dllexport)
 HRESULT __stdcall
 DllCanUnloadNow()
 {
+  initialize_logger();
   ELLE_TRACE_SCOPE("check if the dll can unload (lock: %s)", g_DllLockCounter);
 
   return g_DllLockCounter > 0 ? S_FALSE : S_OK;
@@ -74,6 +115,7 @@ __declspec(dllexport)
 HRESULT __stdcall
 DllUnregisterServer()
 {
+  initialize_logger();
   ELLE_TRACE_SCOPE("unregister server (lock: %s)", g_DllLockCounter);
   return S_OK;
 }
