f3198def8ff8f306323e5c140cd810aa4c58bb06
Antony MECHIN <antony.mechin@infinit.io>
2015-06-05 15:55:22 +0200

    SendView.NoSearchResult: Fix indentation.

--- src/fist-gui-qt/SendView/NoSearchResultWidget.cc
+++ src/fist-gui-qt/SendView/NoSearchResultWidget.cc
@@ -12,27 +12,27 @@ namespace fist
       , _layout(new QVBoxLayout(this))
     {
 
-this->setAutoFillBackground(false);
+      this->setAutoFillBackground(false);
       this->_layout->setSpacing(7);
-this->_layout->setContentsMargins(0, 20, 0, 20);
-auto* title = new QLabel(view::no_result::title::text, this);
-view::no_result::title::style(*title);
-this->_layout->addWidget(title, 0, Qt::AlignCenter);
+      this->_layout->setContentsMargins(0, 20, 0, 20);
+      auto* title = new QLabel(view::no_result::title::text, this);
+      view::no_result::title::style(*title);
+      this->_layout->addWidget(title, 0, Qt::AlignCenter);
 
-auto* subtitle = new QLabel(view::no_result::subtitle::text, this);
-view::no_result::subtitle::style(*subtitle);
-this->_layout->addWidget(subtitle, 0, Qt::AlignCenter);
+      auto* subtitle = new QLabel(view::no_result::subtitle::text, this);
+      view::no_result::subtitle::style(*subtitle);
+      this->_layout->addWidget(subtitle, 0, Qt::AlignCenter);
 
-auto* icon = new QLabel(this);
-icon->setPixmap(QPixmap(":/send/no-results"));
-this->_layout->addSpacing(5);
-this->_layout->addWidget(icon, 0, Qt::AlignCenter);
+      auto* icon = new QLabel(this);
+      icon->setPixmap(QPixmap(":/send/no-results"));
+      this->_layout->addSpacing(5);
+      this->_layout->addWidget(icon, 0, Qt::AlignCenter);
     }
 
     QSize
     NoSearchResultWidget::sizeHint() const
     {
-auto size = this->_layout->minimumSize();
+      auto size = this->_layout->minimumSize();
       return QSize(Super::sizeHint().width(), size.height());
     }
   }
