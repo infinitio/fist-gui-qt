4b7ddcd383a446f7710df34e1db2b8fb3595d3af
Antony MECHIN <antony.mechin@infinit.io>
2015-06-05 15:25:19 +0200

    SearchResults: Rework attributes.

--- src/fist-gui-qt/SendView/OwnDeviceSearchResult.cc
+++ src/fist-gui-qt/SendView/OwnDeviceSearchResult.cc
@@ -5,32 +5,28 @@ namespace fist
 {
   namespace sendview
   {
-    OwnDeviceSearchResult::OwnDeviceSearchResult(
-      fist::model::User const& model,
-      fist::model::Device const& device,
-      QWidget* parent)
-      : SearchResultWidget(model, false, parent)
+    OwnDeviceSearchResult::OwnDeviceSearchResult(fist::model::User const& user,
+                                                 fist::model::Device const& device,
+                                                 bool selected,
+                                                 QWidget* parent)
+      : Super(selected, parent)
       , _device(device)
+      , _id(user.id())
     {
       this->_avatar->set_avatar(gui::icon_provider().icons().at(device.type()));
       this->_fullname->setText(device.name());
-
-      disconnect(&model, SIGNAL(avatar_updated()),
-                 this, SLOT(_on_avatar_updated()));
     }
 
     void
     OwnDeviceSearchResult::_selected()
     {
-      emit selected_device(this->_model.id(),
-                           this->_device.id());
+      emit selected_device(this->id(), this->_device.id());
     }
 
     void
     OwnDeviceSearchResult::_unselected()
     {
-      emit unselected_device(this->_model.id(),
-                             this->_device.id());
+      emit unselected_device(this->id(), this->_device.id());
     }
   }
 }
