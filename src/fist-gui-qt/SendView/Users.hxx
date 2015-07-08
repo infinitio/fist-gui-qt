88148f8f590e80cec7668886afff312668e43e3d
Antony MECHIN <antony.mechin@infinit.io>
2015-07-08 12:47:46 +0200

    SendView.Users: Remove faulty log on debian.

--- src/fist-gui-qt/SendView/Users.hxx
+++ src/fist-gui-qt/SendView/Users.hxx
@@ -3,8 +3,6 @@
 
 # include <algorithm>
 
-# include <elle/log.hh>
-
 # include <fist-gui-qt/SendView/Users.hh>
 
 namespace fist
@@ -15,13 +13,10 @@ namespace fist
     bool
     Users::_in_recipients(T const& r) const
     {
-      ELLE_LOG_COMPONENT("infinit.FIST.SendView.Users");
-      ELLE_DEBUG_SCOPE("search %s in %s", r, this->_recipients);
       return std::find_if(this->_recipients.begin(),
                           this->_recipients.end(),
                           [&] (Recipient const& recipient)
                           {
-                            ELLE_DUMP("recpient: %s", recipient);
                             return recipient == r;
                           }) != this->_recipients.end();
     }
