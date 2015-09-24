c0757f2d903dd8674318fba5b8838758e89a4fc6
Antony MECHIN <antony.mechin@infinit.io>
2015-09-24 15:37:31 +0200

    CustomEvents.AccountUpdated: Add logs.

--- src/fist-gui-qt/CustomEvents/AccountUpdated.cc
+++ src/fist-gui-qt/CustomEvents/AccountUpdated.cc
@@ -1,11 +1,14 @@
 #include <fist-gui-qt/CustomEvents/Events.hh>
 #include <fist-gui-qt/CustomEvents/AccountUpdated.hh>
 
+ELLE_LOG_COMPONENT("infinit.FIST.events.AccountUpated")
+
 namespace fist
 {
   AccountUpdated::AccountUpdated(::Account const& account)
     : QEvent(ACCOUNT_UPDATED)
     , _account(account)
   {
+    ELLE_DEBUG("%s: account updated: %s", *this, account);
   }
 }
